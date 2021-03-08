b3922507a744ce952ae93ac82b41bda8e9a146ef
Da-Mell <Damellfaines8@gmail.com>
2021-03-08 16:38:22 -0500

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Review.destroy_all
+Favorite.destroy_all
+User.destroy_all
+Podcast.destroy_all
+Mood.destroy_all
+
+
+moods = [
+  {feeling: "Happy"},
+  {feeling: "Sad"},
+  {feeling: "Anxious"},
+  {feeling: "Mysterious" }
+]
+
+moods.each {|mood| Mood.create(mood)}
+
+users = [
+  {username: "Kamala Khan", password: "abc123",email: "Kamala@yahoo.com"},
+  {username: "Doreen Green", password: "def456", email: "Doreen@yahoo.com"},
+  {username: "Gwen Stacy", password: "ghi789",email: "Gwen@yahoo.com" },
+  {username: "Janet Van Dyne", password: "jkl012",email: "Janet@yahoo.com"},
+  {username: "Wanda Maximoff", password: "mno345",email: "Wanda@yahoo.com"}
+]
+
+users.each { |user| User.create(user) }
+
+
+
+podcasts = [
+    {title: "Joe Budden Podcast", description: "Hip-hop/Comedy", length: "long", mood_id: 1},
+    {title: "Joe Rogan", description: "Comedy/Conspiracy", length:"long", mood_id: 4 },
+    {title: "Science Time", description: "Sci-Fi/Short stories", length: "short", mood_id:4},
+    {title: " Plants with bob ", description: "Nature/Outdoor topics ", length:"average", mood_id:2}
+  ]
+  
+  podcasts.each {|podcast| Podcast.create(podcast)}
+  
+  reviews = [ "I really loved this episode, keep it up !! ", "This Sh*t was trash !!! ", "Cant wait for next Week !! ", "Unsubscribed this was very offensive !!!", "I really loved this episode, keep it up !! "
+  ]
+
+
+5.times do 
+Review.create(content: reviews.to_a.sample, user: User.all.to_a.sample, podcast: Podcast.all.to_a.sample)
+end
+
+5.times do 
+    Favorite.create(user: User.all.to_a.sample, podcast: Podcast.all.to_a.sample)
+end
+
+puts "SEEDED 🌿"
