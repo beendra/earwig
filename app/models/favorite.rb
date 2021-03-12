ebfbb950fb90cd6c68f4709eab9ee7240b09b942
Da-Mell <Damellfaines8@gmail.com>
2021-03-12 12:57:45 -0500

    only needs read me

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :podcast
-
+  validates  :user_id, uniqueness: {scope: :podcast_id}
   def delete_faves
     @remove = @current_user.favorited_podcasts
     @remove.each do |r| 
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,18 @@ users = [
   {username: "Doreen Green", password: "def456", email: "Doreen@yahoo.com"},
   {username: "Gwen Stacy", password: "ghi789",email: "Gwen@yahoo.com" },
   {username: "Janet Van Dyne", password: "jkl012",email: "Janet@yahoo.com"},
-  {username: "Wanda Maximoff", password: "mno345",email: "Wanda@yahoo.com"}
+  {username: "Wanda Maximoff", password: "mno345",email: "Wanda@yahoo.com"},
+  {username: "Bruce Banner", password: "abc123",email: "Bruce@yahoo.com"},
+  {username: "Thor", password: "abc123",email: "Kamala@yahoo.com"},
+  {username: "Miles Morales", password: "abc123",email: "Miles@yahoo.com"},
+  {username: "Stephen Strange", password: "abc123",email: "Strange@yahoo.com"},
+  {username: "Tony Starks", password: "abc123",email: "Tony@yahoo.com"},
+  {username: "T'Challa", password: "abc123",email: "Challa@yahoo.com"},
+  {username: "Clint Barton", password: "abc123",email: "Clint@yahoo.com"},
+  {username: "Steven Rogers", password: "abc123",email: "Steve@yahoo.com"},
+  {username: "Wade Wilson", password: "abc123",email: "Wade@yahoo.com"},
+  {username: "Matt Murdock", password: "abc123",email: "Matt@yahoo.com"}
+
 ]
 
 users.each { |user| User.create(user) }
@@ -36,20 +47,32 @@ podcasts = [
     {title: "Joe Budden Podcast", description: "Hip-hop/Comedy", length: "Long", mood_id: 1},
     {title: "Joe Rogan", description: "Comedy/Conspiracy", length: "Long", mood_id: 4 },
     {title: "Science Time", description: "Sci-Fi/Short stories", length: "Short", mood_id:4},
-    {title: " Plants with bob ", description: "Nature/Outdoor topics ", length: "Average", mood_id:2}
+    {title: " Plants with bob", description: "Nature/Outdoor topics ", length: "Average", mood_id:2},
+    {title: "Bully and The Beast", description: "Romance/Comedy", length: "Average", mood_id: 1},
+    {title: "The Brilliant Idiots", description: "Pop-Culture/Comedy", length: "Long", mood_id: 1},
+    {title: "Home Cooking", description: "Cooking", length: "Short", mood_id: 2},
+    {title: "Dead Eyes", description: "Murder-Mystery", length: "Short", mood_id: 3},
+    {title: "The Daily", description: "News", length: "Long", mood_id: 2},
+    {title: "Outsiders", description: "Murder-Mystery", length: "Short", mood_id: 4},
+    {title: "Staying In With Emily and Kumail", description: "Pop-culture", length: "Average", mood_id: 2},
+    {title: "Pardon My Take", description: "Sports", length: "Average", mood_id: 2},
+    {title: "All the Smoke", description: "Sports", length: "Long", mood_id: 1},
+    {title: "The Bill Simmons Podcast", description: "Sports", length: "Long", mood_id: 3},
+    {title: "The Lowe Post", description: "Sports", length: "Average", mood_id: 4},
+
   ]
   
   podcasts.each {|podcast| Podcast.create(podcast)}
   
-  reviews = [ "I really loved this episode, keep it up !! ", "This Sh*t was trash !!! ", "Cant wait for next Week !! ", "Unsubscribed this was very offensive !!!", "I really loved this episode, keep it up !! "
-  ]
+  reviews = [ "I really loved this episode, keep it up !! ", "This Sh*t was trash !!! ", "Cant wait for next Week !! ", "Unsubscribed this was very offensive !!!", "I really loved this episode, keep it up !! ",
+  "Great Show Guys","Hated it","turned it off","Cant wait for tomorrow","I Love to listen while i code","I almost got fired for listening at work lol"]
 
 
-5.times do 
+40.times do 
 Review.create(content: reviews.to_a.sample, user: User.all.to_a.sample, podcast: Podcast.all.to_a.sample)
 end
 
-5.times do 
+40.times do 
     Favorite.create(user: User.all.to_a.sample, podcast: Podcast.all.to_a.sample)
 end
 
